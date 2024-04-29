fdb9bdcc67da53afd44e8c796c32f6a196d13eb4
enrique_escalante-notario <enriquescalante@gmail.com>
2024-04-29 13:29:59 -0600

    Update compilation configuration files

--- Config/config.h
+++ Config/config.h
@@ -11,7 +11,7 @@
 #define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
+/* #undef HAVE_MEMORY_H */
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -42,7 +42,7 @@
 #define PACKAGE "orbifolder"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "orbifolder@projects.hepforge.org "
+#define PACKAGE_BUGREPORT "orbifolder@projects.hepforge.org"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "Orbifolder"
